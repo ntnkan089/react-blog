dde1db2799ce45d76f7bce5bf2ea865066597539
ntnkan089@gmail.com <ntnkan089@gmail.com>
2023-12-18 15:19:57 -0800

    ok

--- src/About.js
+++ src/About.js
@@ -3,6 +3,7 @@ const About = () => {
         <main className='About'>
             <h2>About</h2>
             <p style={{ marginTop: "1rem" }}>This is a blog app. Feel free to use it!</p>
+            <p style={{ marginTop: "1rem" }}>Click on "Post" to create a post.</p>
         </main>
     )
 }
